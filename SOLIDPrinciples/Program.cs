c7369f676b6b8e372c0dbfddae2545ef7690e44e
Baris Tuncel <cemrebaristuncel@hotmail.com>
2023-07-20 23:20:18 +0300

    Letter I Example

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -7,8 +7,36 @@ namespace SOLIDPrinciples
     {
         static void Main(string[] args)
         {
-            // You can find each of SOLID examples in different branches.
+            CarnivoreAnimals carnivoreAnimals = new CarnivoreAnimals();
+            HerbivoreAnimals herbivoreAnimals = new HerbivoreAnimals();
+
+            carnivoreAnimals.FeedCarnivore();
+            herbivoreAnimals.FeedHerbivore();
         }
     }
 }
+public interface IFeedCarnivore
+{
+    void FeedCarnivore();
+}
 
+public interface IFeedHerbivore
+{
+    void FeedHerbivore();
+}
+
+public class CarnivoreAnimals : IFeedCarnivore
+{
+    public void FeedCarnivore()
+    {
+        Console.WriteLine("Carnivore animals are being fed with meat");
+    }
+}
+
+public class HerbivoreAnimals : IFeedHerbivore
+{
+    public void FeedHerbivore()
+    {
+        Console.WriteLine("Herbivore animals are being fed with grass");
+    }
+}
